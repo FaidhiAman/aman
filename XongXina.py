982477309e4e5ee20908e69704b752e240b6fae8
FaidhiAman <115853702+FaidhiAman@users.noreply.github.com>
2022-10-22 13:27:52 +0800

    Add files via upload

---
+++ XongXina.py
@@ -0,0 +1,5 @@
+import telepot
+
+bot = telepot.Bot("5789390248:AAEk3WTWIuSTGNtfCjBteK7GOe1xr9QbPCg")
+
+bot.sendPhoto(chat_id="1745183471", photo=("https://i.scdn.co/image/ab6761610000e5ebe1408498d7f528e3671616b1"))
